c5f47f75ea243490368b56082c0ba78a750be016
Cyfralus <Cyfralus@DESKTOP-VPABBMO>
2018-12-14 17:29:24 +0400

    Добавьте файлы проекта.

---
+++ Lottery/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Lottery.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID ="output" runat ="server"></asp:Label>
+            <table border="1">
+            <%
+                int count = members.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    Response.Write("<tr>");
+                    Response.Write(String.Format("<th>{0}</th>", members[i].id));
+                    Response.Write(String.Format("<th>{0}</th>", members[i].name));
+                    Response.Write(String.Format("<th>{0}</th>", members[i].winner));
+                    Response.Write("</tr>");
+                }
+            %>
+            </table>
+        </div>
+        <%-- <script type ="text/javascript">
+            $(document).ready(function () {
+                setInterval(function () {
+                    $('#output').html(new Date($.now()));
+                }, 1000)
+            });
+        </script> --%>
+    </form>
+</body>
+</html>
---
+++ Lottery/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lottery
+{
+    public class Person
+    {
+        public int id;
+        public string name;
+        public bool winner;
+
+        public Person(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+            DefineWinner();
+        }
+
+        public void DefineWinner()
+        {
+            Random rnd = new Random();
+            this.winner = Convert.ToBoolean(rnd.Next(0, 2));
+        }
+    }
+
+    public partial class Default : System.Web.UI.Page
+    {
+        public List<Person> members = new List<Person>();
+
+        Person member_1 = new Person(1, "Иванов Иван Иванович");
+        Person member_2 = new Person(2, "Петров Петр Петрович");
+        Person member_3 = new Person(3, "Петренко Татьяна Ивановна");
+        Person member_4 = new Person(4, "Красильников Лазарь Николаевич");
+        Person member_5 = new Person(5, "Ершов Евгений Федосеевич");
+        Person member_6 = new Person(6, "Савин Олег Лукьевич");
+
+        void AddMembersToList()
+        {
+            members.Add(member_1);
+            members.Add(member_2);
+            members.Add(member_3);
+            members.Add(member_4);
+            members.Add(member_5);
+            members.Add(member_6);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            AddMembersToList();
+            foreach (Person member in members)
+                member.DefineWinner();
+
+        }
+    }
+}
